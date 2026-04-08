15ed277bd7eb2f4005d3f13e7fb8f86274ba7ed4
juanbetancourt01 <camilin1869@gmail.com>
2026-04-08 07:40:12 -0500

    punto1.main.cpp

--- punto1/punto1.main.cpp
+++ punto1/punto1.main.cpp
@@ -1,1 +1,20 @@
-int main ()
+#include <iostream>
+
+void intercambiar(int* a, int* b) {
+    int temp = *a;
+    a =b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+
+    std::cout << "Ingrese dos numeros: ";
+    std::cin >> x >> y;
+
+    intercambiar(&x, &y);
+
+    std::cout << "Valores intercambiados: " << x << " " << y << std::endl;
+
+    return 0;
+}
