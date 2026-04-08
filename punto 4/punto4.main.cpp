df987808995ebe5c27b1416b18a71976dc422f68
juanbetancourt01 <camilin1869@gmail.com>
2026-04-08 08:23:29 -0500

    punto4.main.cpp

--- punto 4/punto4.main.cpp
+++ punto 4/punto4.main.cpp
@@ -10,7 +10,7 @@ int triplicar(int x) {
 
 void aplicar(int* arr, int n, int (*func)(int)) {
     for (int i = 0; i < n; i++) {
-        (arr + i) = func((arr + i));
+        *(arr + i) = func(*(arr + i));
     }
 }
 
@@ -21,14 +21,14 @@ int main() {
     int* arr = new int[n];
 
     for (int i = 0; i < n; i++) {
-        std::cin >> (arr + i);
+        std::cin >> *(arr + i);
     }
 
     aplicar(arr, n, duplicar);
 
     std::cout << "Resultado:\n";
     for (int i = 0; i < n; i++) {
-        std::cout <<(arr + i) << " ";
+        std::cout <<*(arr + i) << " ";
     }
 
     delete[] arr;
